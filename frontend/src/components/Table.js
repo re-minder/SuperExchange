e93642b600bff1e0194b59995374e9a1f77afe22
supatomic <kngoyal.cs@gmail.com>
2021-06-28 12:41:34 -0400

    add generic Table template

---
+++ frontend/src/components/Table.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+class Table extends Component {
+    constructor(props) {
+        super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+        this.state = {
+            ...this.props,
+        };
+    }
+
+   renderTableData() {
+        return this.state.rows.map((row, index) => {
+            var rowArray = [];
+
+            for (var i=1; i<this.state.headerLength; i++) {
+                rowArray.push(<td>{row[this.state.header[i]]}</td>)
+            }
+            return (
+                <tr key={row[this.state.header[0]]}>
+                    {rowArray}
+                </tr>
+            )
+        })
+    }
+
+    renderTableHeader() {
+        let header = this.state.header.slice(1,);
+        return header.map((key, index) => {
+           return <th key={index}>{key.toUpperCase()}</th>
+        })
+     }
+  
+     render() {
+        return (
+           <div>
+              <table id="table">
+                 <tbody>
+                    <tr>{this.renderTableHeader()}</tr>
+                    {this.renderTableData()}
+                 </tbody>
+              </table>
+           </div>
+        )
+     }
+}
+
+export default Table
